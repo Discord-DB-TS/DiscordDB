8f188f5f788c6ef27fa7138d9d0a9db77c84cd39
saphirrc <jeremilauzon@gmail.com>
2022-06-15 15:35:54 -0400

    feat: SITTEEE
    Marche pas bien
    
    Donc arrange toi

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -3,6 +3,7 @@
     <ul>
       <li><a href="/"></a>Discord DB</li>
     </ul>
+    <p>DiscordDB est une base de données révolutionnaire.<br>Les informations sont stocker via une nouvelle technique découvert par deux chercheur de l'équipe de recherche.<br>Discord permet de stocker les données en ligne gratuitement !</p>
     <img src="~/assets/img/logoDiscord.png" />
   </div>
 </template> 
